caf62e3939831e1b5798a377b02c14574f949c61
Guilherme Pereira de Freitas <gfreitas@continuum.io>
2017-08-15 18:58:45 -0500

    Add func to extend sklearn.datasets.make_* funcs
    Try this:
    
        import xarray_filters.datasets as ds
        import pandas as pd
        help(ds.make_blobs)   # note argument list and docstring
        X, y = ds.make_blobs(astype=pd.DataFrame)
    
    X, y should be dataframes, not arrays. Note that y should be a Series
    for things to work correctly with sklearn.
    
    The example above shows where we are going: both docstring and signature
    are extensions of the corresponding sklearn.datasets function.
    
    Right now the only extra functionality is the `astype` keyword. We
    should polish that a bit, and include other keywords. Like "shape", or
    named dimensions, etc. This might get more complicated and require a
    refactor, but the current code shows what I'd like to get: make it
    clear (through docstring and signature) that we are just extending
    sklearn functions.
    
    The challenge is to keep the code readable too. Right now, the resulting
    module that is imported is more understandable than the code itself.
    Suggestions welcome.

--- xarray_filters/datasets.py
+++ xarray_filters/datasets.py
@@ -1,9 +1,56 @@
+"""
+This module contains a routines to convert the output of
+
+    sklearn.datasets.make_blobs
+    sklearn.datasets.make_regression
+    sklearn.datasets.make_classification
+
+to the following containers
+
+    xarray.Dataset
+    numpy.array
+    pandas.DataFrame
+
+The sklearn functions all output a tuple (X, y) where
+
+    X is a n_samples by n_features numpy.array of features
+    y is a n_samples numpy.array of labels
+
+The reasons to wrap the sklearn routines are:
+
+- We want new types easily (xarray.Dataset, etc.)
+- We want multidimensional features.
+- We want to generate sample weights together with synthetic data.
+
+Motivating usecases:
+
+- Generate data to test models that predict temperature or humidity at
+  3d-spatial coordinates and time based on earlier measurements at nearby/same
+  coordinates.
+
+Implementation Notes
+--------------------
+
+The final routines should be drop-in replacements for the sklearn functions,
+but with extra keyword arguments to address the needs outlined above. The
+public interface of this module should be only the `make_*` routines; all other
+names should have a leading underscore.
+
+All exposed functions should have useful help strings, and an informative
+signature (no *args, **kwargs).
+
+
+"""
+
+
+
 from collections import Sequence, OrderedDict
-from functools import partial
+from functools import partial, wraps
 
 import numpy as np
 import sklearn.datasets as skdatasets
 import xarray as xr
+import inspect
 
 
 NON_SKLEARN_KWARGS = ('x_output_type',
@@ -16,152 +63,65 @@ NON_SKLEARN_KWARGS = ('x_output_type',
 
 DEFAULT_SHAPE = (50, 50)
 
-ds = dir(skdatasets)
 
-_datasets = {f: getattr(skdatasets, f)
-             for f in ds if f[0] != '_' and f != 'samples_generator'}
-_make_funcs = {k: v for k, v in _datasets.items() if k.startswith('make_')}
-_load_funcs = {k: v for k, v in _datasets.items() if k.startswith('load_')}
-_fetch_funcs = {k: v for k, v in _datasets.items() if k.startswith('fetch_')}
+def flexible_out(skl_make_data_func):
+    """Add keyword arguments to skl_make_data_func.
 
+    Note: a decorator doesn't solve this. It could add the functionality, but
+    it would not change the docstring or the signature of the function, as it
+    would keep the original one from the wrapped function from sklearn. So we
+    have to do the process manually.
+    """
+    def wrapper(*args, astype=None, **kwargs):
+        X, y = skl_make_data_func(*args, **kwargs)
+        if astype:
+            Xcast = astype(X)
+            ycast = astype(y) 
+            return Xcast, ycast
+        else:
+            return X, y
 
-OUTPUT_TYPES = {'numpy:ndarray': ['numpy', 'ndarray',
-                                  'numpy:ndarray', np.ndarray],
-                'xarray:Dataset': ['Dataset', 'xarray:Dataset', xr.Dataset]}
+    # We now have two tasks: (1) fix the docstring and (2) fix the signature of `wrapper`.
+    # Task 1 of 2: fixing the docstring of `wrapper`
+    preamble_doc = """Like {funcname}, but with extra keyword parameters.
 
-def _find_type(output_type):
-    for k, v in OUTPUT_TYPES.items():
-        if output_type in v:
-            return k
-
-def _split_sklearn_out(sklearn_out, make_sample_weight=None, **kwargs):
-    if isinstance(sklearn_out, tuple) and len(sklearn_out) == 2:
-        X, y = sklearn_out
-    else:
-        X = sklearn_out
-        y = None
-    if make_sample_weight:
-        sample_weight = make_sample_weight(X, y, **kwargs)
-    else:
-        sample_weight = None
-    return X, y, sample_weight
-
-
-def _coords_from_other_types(coords):
-    if isinstance(coords, dict):
-        return coords.__getitem__
-    if (isinstance(coords, Sequence) and all(isinstance(c, Sequence) for c in coords)):
-        coords = dict(coords)
-        return coords.__getitem__
-    return coords.__getattr__
-
-
-
-def _X_to_type(item, typ, dims, shape):
-    if typ == 'xarray:Dataset':
-        X_data = OrderedDict()
-        coords = OrderedDict(tuple((d, np.arange(s)) for d, s in zip(dims, shape)))
-        for j in range(item.shape[1]):
-            arr = xr.DataArray(item[:, j].reshape(shape),
-                               coords=coords,
-                               dims=dims)
-            X_data['band_' + str(j)] = arr
-        return xr.Dataset(X_data)
-
-def _y_to_type(*args, **kwargs):
-    return args[0]
-
-_sw_to_type = _y_to_type
-
-def sklearn_output_as_format(sklearn_out,
-                             x_output_type='xarray:Dataset',
-                             y_output_type='numpy:ndarray',
-                             sample_weight_output_type='numpy:ndarray',
-                             shape=None,
-                             as_flat=False,
-                             dims=None,
-                             reshaper=None,
-                             make_sample_weight=None,
-                             **kwargs):
-    """Make dataset based on the output of a sklearn.dataset function.
-
-    Parameters
-    ----------
-    sklearn_out: output from a sklearn.datasets datagen function
-    x_output_type: string (default: 'xarray:Dataset')
-    y_output_type: string (default: 'numpy:ndarray')
-    sample_weight_output_type: type of sample weights (default: 'numpy:ndarray')
-    shape: tuple, optional
-        [TODO] Currently required in code. Either require in signature or do
-        something else but raise an error when the default None value is
-        passed.
-    as_flat: boolean, optional
-    dims: tuple of strings, optional
-    reshaper: type? [TODO] unused in code
-    make_sample_weight: function
-        See _split_sklearn_out.
+    Extra Parameters
+    ----------------
+    astype: type
+        one of pandas.DataFrame, xarray.Dataset, numpy.array. Has to be passed
+        by keyword.
 
     Returns
     -------
-    X2: xarray.DataSet of features
-    y2: 1-dimensional numpy.array of labels
-    sample_weight2: 1-dimensional numpy.array of sample weights, same shape as y2
+    X: feature matrix
+        Like {funcname}, but with type given by astype.
+    y: labels vector
+        Like {funcname}, but with type given by astype.
 
-    """
-    if not shape:
-        raise ValueError('Expected shape keyword argument')
+    See the docs of the sklearn function below
+    ---
 
-    typs = (x_output_type, y_output_type, sample_weight_output_type)
-    x_typ, y_typ, sw_output_type = (_find_type(t) for t in typs)
-    X, y, sample_weight = _split_sklearn_out(sklearn_out,
-                                             make_sample_weight=make_sample_weight,
-                                             x_output_type=x_output_type,
-                                             y_output_type=y_output_type,
-                                             dims=dims,
-                                             shape=shape,
-                                             as_flat=as_flat,
-                                             reshaper=reshaper,
-                                             **kwargs)
-    X2 = _X_to_type(X, x_typ, dims, shape)
-    y2 = _y_to_type(y, y_typ, dims, shape)
-    sample_weight2 = _sw_to_type(sample_weight, sw_output_type, dims, shape)
-    return X2, y2, sample_weight2
+    """.format(funcname=('sklearn.datasets.' + skl_make_data_func.__code__.co_name))
+    wrapper.__doc__ = preamble_doc + skl_make_data_func.__doc__
+    # Task 2 of 2: fixing the signature of `wrapper`
+    argspec = inspect.getfullargspec(skl_make_data_func)
+    params = [inspect.Parameter(name=pname, kind=inspect.Parameter.POSITIONAL_OR_KEYWORD, default=pdefault)
+              for (pname, pdefault) in zip(argspec.args, argspec.defaults)]
+    params += [inspect.Parameter(name='astype',
+        kind=inspect.Parameter.KEYWORD_ONLY, default=None)]
+    wrapper.__signature__ = inspect.Signature(params)
+    wrapper.__name__ = skl_make_data_func.__name__
+    return wrapper
 
+make_blobs = flexible_out(skldatasets.make_blobs)
 
-def make_base(sklearn_func, *args, **kwargs):
-    """Make dataset based on sklearn datagen function.
-
-    Parameters    
-    ----------
-    sklearn_func : function from sklearn.datasets
-        Examples: make_blobs, make_regression, etc.
-    args : positional arguments passed to sklearn_func
-    kwargs : keyword arguments passed to sklearn_func and sklearn_output_as_format
-        You may also pass a 'shape' (tuple of integers) and corresponding
-        dimension names 'dims' (tuple of strings, same size as 'shape'). This
-        follows the xarray.DataArray conventions. 
-
-    Returns
-    -------
-    X: xarray.DataSet of features
-    y: 1-dimensional numpy.array of labels
-    sample_weight: 1-dimensional numpy-array of sample weights, same shape as y
-        See documentation of [TODO] for this.
-
-    See Also
-    --------
-    sklearn_output_as_format
-    """
-    if not kwargs.get('shape'):
-        kwargs['shape'] = DEFAULT_SHAPE
-    kw = {k: v for k,v in kwargs.items() if k not in NON_SKLEARN_KWARGS}
-    kw['n_samples'] = np.prod(kwargs['shape'])
-    sklearn_out = sklearn_func(*args, **kw)
-    if not kwargs.get('dims'):
-        kwargs['dims'] = tuple('xyzt')[:len(kwargs['shape'])]
-    X, y, sample_weight = sklearn_output_as_format(sklearn_out, **kwargs)
-    return X, y, sample_weight
-
-
-make_regression = partial(make_base, skdatasets.make_regression)
-make_blobs = partial(make_base, skdatasets.make_blobs)
+# [WIP] Try this: 
+#
+# [WIP]     import pandas as pd
+# [WIP]     help(make_blobs)   # note argument list and docstring
+# [WIP]     make_blobs(astype=pd.DataFrame)
+#
+# [WIP] This shows where we are going. The idea is now to extend that to extra
+# [WIP] keyword arguments. The challenge is to keep the code readable too. Right now,
+# [WIP] the resulting module that is imported is more understandable than the code
+# [WIP] itself.
