99cede58f077f33b7db08a2ac6dc33c458907785
Peter Steinberg <psteinberg@continuum.io>
2017-08-25 11:41:24 -0700

    template - notes for tests of make_blobs, make_regression and similar funcs

---
+++ xarray_filters/tests/test_datasets.py
@@ -0,0 +1,32 @@
+'''
+
+Programmatically test that each of the following
+works as expected.  Use random seed arg to control.
+
+ * Call the original function with seed
+ * Same seed - call the wrapped one
+ *   Try different output options
+ *   Ensure you get right data structure
+ *   Check things like non-defauts, e.g. layers is None or not None
+ *
+datasets.make_biclusters(shape, n_clusters) Generate an array with constant block diagonal structure for biclustering.
+datasets.make_blobs([n_samples, n_features, …]) Generate isotropic Gaussian blobs for clustering.
+datasets.make_checkerboard(shape, n_clusters)   Generate an array with block checkerboard structure for biclustering.
+datasets.make_circles([n_samples, shuffle, …])  Make a large circle containing a smaller circle in 2d.
+datasets.make_classification([n_samples, …])    Generate a random n-class classification problem.
+datasets.make_friedman1([n_samples, …]) Generate the “Friedman #1” regression problem
+datasets.make_friedman2([n_samples, noise, …])  Generate the “Friedman #2” regression problem
+datasets.make_friedman3([n_samples, noise, …])  Generate the “Friedman #3” regression problem
+datasets.make_gaussian_quantiles([mean, …]) Generate isotropic Gaussian and label samples by quantile
+datasets.make_hastie_10_2([n_samples, …])   Generates data for binary classification used in Hastie et al.
+datasets.make_low_rank_matrix([n_samples, …])   Generate a mostly low rank matrix with bell-shaped singular values
+datasets.make_moons([n_samples, shuffle, …])    Make two interleaving half circles
+datasets.make_multilabel_classification([…])    Generate a random multilabel classification problem.
+datasets.make_regression([n_samples, …])    Generate a random regression problem.
+datasets.make_s_curve([n_samples, noise, …])    Generate an S curve dataset.
+datasets.make_sparse_coded_signal(n_samples, …) Generate a signal as a sparse combination of dictionary elements.
+datasets.make_sparse_spd_matrix([dim, …])   Generate a sparse symmetric definite positive matrix.
+datasets.make_sparse_uncorrelated([…])  Generate a random regression problem with sparse uncorrelated design
+datasets.make_spd_matrix(n_dim[, random_state]) Generate a random symmetric, positive-definite matrix.
+datasets.make_swiss_roll([n_samples, noise, …]) Generate a swiss roll dataset.
+'''
