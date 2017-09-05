ccdafea025f3fa09136d111c0960c9ac124cdc9c
gbrener <gbrener@continuum.io>
2017-09-05 13:00:32 -0500

    Add conda.recipe, get conda to build, add versioneer files, etc

--- xarray_filters/__init__.py
+++ xarray_filters/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 from xarray_filters.constants import *
 from xarray_filters.func_signatures import filter_args_kwargs
 from xarray_filters.mldataset import *
