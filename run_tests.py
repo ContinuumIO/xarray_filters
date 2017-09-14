48b0dfd5a2e77d4a48a0fd50c77814abb1ccd5ef
Guilherme Pereira de Freitas <gfreitas@anaconda.com>
2017-09-14 15:27:53 -0500

    Improve run_tests.py
    It now clearly reports the status of each test.
    
    That makes it easy for the user to pick the environment he needs to work
    on.

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import time
 import logging
+import datetime
 
 
 
@@ -26,11 +27,11 @@ def create_env(env_fpath):
 
 def run_tests(env_name):
     """Run tests in the conda env of env_fpath."""
-    cmd = 'source activate {} && pytest --cov && source deactivate'.format(env_name)
+    test_cmd = 'pytest --cov'
+    cmd = 'source activate {} && {} && source deactivate'.format(env_name, test_cmd)
     with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
-        retcode = proc.poll()
         out = proc.stdout.read()
-    return retcode, out.decode(encoding='utf-8')
+    return out.decode(encoding='utf-8')
 
 def make_test_report(env_name):
     test_report = {
@@ -45,9 +46,12 @@ def make_test_report(env_name):
         'git_user.email': subprocess.check_output('git config user.email',
             shell=True).decode('utf-8').rstrip()
     }
-    test_report['t0_UTC_ms'] = int(time.time() * 1000)
-    test_report['retcode'], test_report['report'] = run_tests(env_name)
-    test_report['t1_UTC_ms'] = int(time.time() * 1000)
+    test_report['t0_UTC'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d_%H%M%S')
+    test_report['report'] = run_tests(env_name)
+    test_report['t1_UTC'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d_%H%M%S')
+    test_report['status'] = [l.rstrip()
+            for l in test_report['report'].splitlines()
+            if (l.startswith('=============') and 'seconds' in l)][-1]
     return test_report
 
 if __name__ == "__main__":
@@ -68,21 +72,19 @@ if __name__ == "__main__":
     for (testn, env_fpath) in enumerate(env_files):
         with open(env_fpath) as f:
             env_data = yaml.load(f)
-        print("Running test {:d} out of {:d}".format(testn + 1, ntests))
-        print("Environment {} ".format(env_data['name']))
-        time.sleep(0.5)
+        print("Running test {:d} out of {:d}. Environment: {}".format(
+            testn + 1, ntests, env_data['name']))
         if not exists_env(env_data['name']):
             create_env(enf_fpath)
         test_report = make_test_report(env_data['name'])
-        test_log_fname = '_'.join([env_data['name'], str(test_report['t0_UTC_ms']), 'test.log'])
+        test_log_fname = '_'.join([env_data['name'], str(test_report['t0_UTC']), 'test.log'])
         report_path = os.path.join(test_logs_dir, test_log_fname)
         with open(report_path, mode='w') as f:
             json.dump(test_report, f, indent=4)
-        t0, t1, retcode = [test_report.get(x) for x in ('t0_UTC_ms', 't1_UTC_ms', 'retcode')]
-        print('DONE. Test finished in {:f} seconds with return code {}'.format(
-            (t1 - t0) / 1000., str(retcode)))
-        print('Test log in {}\n'.format(report_path))
-    print("\nRun\n")
-    print("    echo -e $(jq '.report' test_logs/<logfile>)")
-    print("\nto see the test report. Additional metadata stored in the json.\n\n")
+        t0, t1, retcode = [test_report.get(x) for x in ('t0_UTC', 't1_UTC', 'retcode')]
+        print('Test log in {}'.format(report_path))
+        print(test_report['status'] + '\n')
+    print("All tests finished. Run\n")
+    print("    echo -e $(jq '.report' <logfile_path>)")
+    print("\nto see the test report. Additional metadata stored in the log file json.")
 
