64c9bf4d3c97efff324c532e956363659542d256
Peter Steinberg <psteinberg@continuum.io>
2017-10-16 18:57:15 -0700

    make matplotlib optional

--- xarray_filters/plotting_helpers.py
+++ xarray_filters/plotting_helpers.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 '''
 ------------------------------------
 
-``earthio.filters.plotting_helpers``
+``xarray_filters.filters.plotting_helpers``
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 '''
