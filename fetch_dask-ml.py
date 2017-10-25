e67571776b680d066a6f1f5f0bfdd3373aff6389
gbrener <gbrener@continuum.io>
2017-10-25 17:23:27 -0500

    Fix script permissions, make some cleanups

--- fetch_dask-ml.py
+++ fetch_dask-ml.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import requests
 import jinja2
-import os
-import subprocess
 
 recipe_dpath = './dask-ml'
-if os.path.isdir(recipe_dpath):
+meta_yml_fpath = os.path.join(recipe_dpath, 'meta.yaml')
+if os.path.isfile(meta_yml_fpath):
     print('Recipe exists: skipping download.')
+    sys.exit(0)
 
 meta_yml_url = 'https://raw.githubusercontent.com/TomAugspurger/staged-recipes/dask-ml/recipes/dask-ml/meta.yaml'
 resp = requests.get(meta_yml_url)
 assert resp.status_code == 200, 'Error fetching meta.yaml file'
 template = jinja2.Template(resp.text)
 os.makedirs(recipe_dpath)
-with open(os.path.join(recipe_dpath, 'meta.yaml'), 'w') as fp:
+print('Writing '+meta_yml_fpath)
+with open(meta_yml_fpath, 'w') as fp:
     fp.write(template.render())
